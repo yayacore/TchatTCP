ab41671920c017ebb0f5012cdc1c220837222551
Yaya <pricillia.mendes-freire@laplateforme.io>
2025-03-17 13:33:54 +0100

    indentation fixed

--- serveur/recev.c
+++ serveur/recev.c
@@ -19,24 +19,21 @@ void * traitement_rcv(void *arg)
         int nb_datas = recv(client.socket_fd, &colis, sizeof(t_message), 0); perror("rcv"); // *fd pour accéder à la valeur pointée
 
             
-            if(nb_datas == -1)
-            {
-                printf("erreur rcv//\n");
-                continue;
-            }
+        if(nb_datas == -1)
+        {
+            printf("erreur aucune donnée n'a été récupérée//\n");
+            continue;
+        }
+        
+        if(nb_datas == 0)     // !!!!! pb à fix rcv à l'infini
+        {
+            printf("user gone  :'(\n");
+            nb_datas ++;
+            continue;
+        }
             
-            // //(user.id_room == idroom receptionneur)
-            
-            if(nb_datas == 0)     // !!!!! pb à fix rcv à l'infini
-            {
-                printf("user gone  :'(\n");
-                nb_datas ++;
-                continue;
-                //     // nb_users_quittants ++ ?
-            }
-            
-            for(int i = 0; i < next_client_index; i++) 
-            {
+        for(int i = 0; i < next_client_index; i++) 
+        {
         
             printf("SEND ✅\n");
             send(users[i].socket_fd, &colis, sizeof(t_message), 0); perror("send");
